3f5ca7c67fe8d41c0a5d481fd3bab1257ae674fc
ninja6734 <123981615+ninja6734@users.noreply.github.com>
2024-08-04 23:36:39 +0200

    fixed some bugs for the updated version of the moving system

--- try 1/test2.py
+++ try 1/test2.py
@@ -1,13 +1,14 @@
 board = [[1, 0, 1, 0, 0, 0, 1, 0], [0, 0, 0, -1, 0, 0, 0, 1], [1, 0, -1, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 1, 0, -1], [2, 0, 0, 0, -1, 0, -1, 0], [0, 0, 0, 0, 0, -1, 0, 0], [0, 0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 0, 0, -1, 0, 0]]
 def obtainSingleActionOfPiece(pieceID, lastPositions, actions, First=True):
-    print(actions)
+    
+    
     _actions = actions
     _lastPositions = lastPositions
     _fixed = False
     if(_lastPositions):
-        print(_lastPositions)
+        
         if(type(_lastPositions[0][-1]) == type([])):
-            directions = _lastPositions[0][-1]
+            directions = _lastPositions[0].pop(-1)
             _fixed = True
         else:
             directions = [(1,1),(1,-1),(-1,-1),(-1,1)]
@@ -15,18 +16,23 @@ def obtainSingleActionOfPiece(pieceID, lastPositions, actions, First=True):
                 directions = directions[:2]
             elif(pieceID == -1):
                 directions = directions[2:]
+        
         pos = _lastPositions.pop(0)
+        #TODO: wird nur einmal ausgeführ wegen "return": nur eine richtung wird bedacht
         for dirX,dirY in directions:
             nextX = pos[-1] + dirX
             nextY = pos[-2] + dirY
-            if(0 <= nextX <= 8):
-                if(0 <= nextY <= 8):
+            if(0 <= nextX < 8):
+                if(0 <= nextY < 8):
                     testingSpace = board[nextY][nextX]
                     if(testingSpace == 0):
                         if(abs(pieceID) == 2):
                             #promoted and no take
+                            
                             positions = pos + [nextY,nextX]
-                            _lastPositions.append(positions.append([dirX,dirY]))
+                            
+                            positions.append([[dirX,dirY]])
+                            _lastPositions.append(positions)
                             if(_fixed):
                                 _actions.append(positions[:-1])
                                 return obtainSingleActionOfPiece(pieceID, _lastPositions[:-1], _actions)
@@ -35,26 +41,33 @@ def obtainSingleActionOfPiece(pieceID, lastPositions, actions, First=True):
                                 return obtainSingleActionOfPiece(pieceID, _lastPositions, _actions)
                         elif(First):
                             #not promoted and no take
+                            
                             positions = pos + [nextY,nextX]
                             _actions.append(positions)
                             return obtainSingleActionOfPiece(pieceID, _lastPositions, _actions)
                     elif(not testingSpace/abs(testingSpace) == pieceID/abs(pieceID)):
-                        testingSpace = board[nextY+dirY][nextX+dirX]
-                        if(abs(pieceID) == 2):
-                            #promoted and a take
-                            if(testingSpace == 0):
-                                positions = pos + [nextY, nextX,nextY + dirY, nextX+ dirX]
-                                _lastPositions.append(positions)
-                                _actions.append(positions)
-                                return obtainSingleActionOfPiece(pieceID, _lastPositions, _actions)
-                        else:
-                            #not promoteed and a take
-                            if(testingSpace == 0):
-                                positions = pos + [nextY, nextX , (nextY + dirY), (nextX+ dirX)]
-                                print(pos)
-                                _lastPositions.append(positions)
-                                _actions.append(positions)
-                                return obtainSingleActionOfPiece(pieceID, _lastPositions, _actions, First=False)
+                        
+                        
+                        if(0 <= (nextX+dirX) < 8):
+                            if(0 <= (nextY+dirY) < 8):
+                                testingSpace = board[nextY+dirY][nextX+dirX]
+                                if(abs(pieceID) == 2):
+                                    #promoted and a take
+                                    
+                                    if(testingSpace == 0):
+                                        positions = pos + [nextY, nextX,nextY + dirY, nextX+ dirX]
+                                        _lastPositions.append(positions)
+                                        _actions.append(positions)
+                                        return obtainSingleActionOfPiece(pieceID, _lastPositions, _actions)
+                                else:
+                                    #not promoteed and a take
+                                    
+                                    if(testingSpace == 0):
+                                        positions = pos + [nextY, nextX , (nextY + dirY), (nextX+ dirX)]
+                                        
+                                        _lastPositions.append(positions)
+                                        _actions.append(positions)
+                                        return obtainSingleActionOfPiece(pieceID, _lastPositions, _actions, First=False)
                     else:
                         return obtainSingleActionOfPiece(pieceID, _lastPositions, _actions)
                 return obtainSingleActionOfPiece(pieceID, _lastPositions, _actions)
@@ -67,7 +80,9 @@ def obtainSingleActionOfPiece(pieceID, lastPositions, actions, First=True):
 
     
 def getActionsOfPiece(pieceID,coor):
-    return obtainSingleActionOfPiece(pieceID, [coor], [])
+    actions = obtainSingleActionOfPiece(pieceID, [coor], [])
+    print(f"actions outer: {coor} for {actions}")
+    return actions
 
 
 def getActions(piece):
@@ -80,4 +95,4 @@ def getActions(piece):
     
     return actions
 
-print(getActions(-1))
+print(getActions(1))
--- try 1/test3.py
+++ try 1/test3.py
@@ -1,1 +1,3 @@
-print([1,2,3,4][:-1])
+position = [1,2,3]
+position.append([1,2])
+print(position)
